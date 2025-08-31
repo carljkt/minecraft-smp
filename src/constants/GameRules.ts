549bae9ded7016b08af8747dd8a3628cffd71411
carljkt <carl@tranate.com>
2025-08-31 12:47:10 +0800

    feat: add GameRules typings

---
+++ src/constants/GameRules.ts
@@ -0,0 +1,234 @@
+export const GameRules = [
+	{
+		key: 'allowEnteringNetherUsingPortals',
+		type: 'boolean',
+	},
+	{
+		key: 'allowFireTicksAwayFromPlayer',
+		type: 'boolean',
+	},
+	{
+		key: 'announceAdvancements',
+		type: 'boolean',
+	},
+	{
+		key: 'blockExplosionDropDecay',
+		type: 'boolean',
+	},
+	{
+		key: 'commandBlockOutput',
+		type: 'boolean',
+	},
+	{
+		key: 'commandModificationBlockLimit',
+		type: 'integer',
+	},
+	{
+		key: 'disableElytraMovementCheck',
+		type: 'boolean',
+	},
+	{
+		key: 'disablePlayerMovementCheck',
+		type: 'boolean',
+	},
+	{
+		key: 'disableRaids',
+		type: 'boolean',
+	},
+	{
+		key: 'doDaylightCycle',
+		type: 'boolean',
+	},
+	{
+		key: 'doEntityDrops',
+		type: 'boolean',
+	},
+	{
+		key: 'doFireTick',
+		type: 'boolean',
+	},
+	{
+		key: 'doImmediateRespawn',
+		type: 'boolean',
+	},
+	{
+		key: 'doInsomnia',
+		type: 'boolean',
+	},
+	{
+		key: 'doLimitedCrafting',
+		type: 'boolean',
+	},
+	{
+		key: 'doMobLoot',
+		type: 'boolean',
+	},
+	{
+		key: 'doMobSpawning',
+		type: 'boolean',
+	},
+	{
+		key: 'doPatrolSpawning',
+		type: 'boolean',
+	},
+	{
+		key: 'doTileDrops',
+		type: 'boolean',
+	},
+	{
+		key: 'doTraderSpawning',
+		type: 'boolean',
+	},
+	{
+		key: 'doVinesSpread',
+		type: 'boolean',
+	},
+	{
+		key: 'doWardenSpawning',
+		type: 'boolean',
+	},
+	{
+		key: 'doWeatherCycle',
+		type: 'boolean',
+	},
+	{
+		key: 'drowningDamage',
+		type: 'boolean',
+	},
+	{
+		key: 'enableCommandBlocks',
+		type: 'boolean',
+	},
+	{
+		key: 'enderPearlsVanishOnDeath',
+		type: 'boolean',
+	},
+	{
+		key: 'fallDamage',
+		type: 'boolean',
+	},
+	{
+		key: 'fireDamage',
+		type: 'boolean',
+	},
+	{
+		key: 'forgiveDeadPlayers',
+		type: 'boolean',
+	},
+	{
+		key: 'freezeDamage',
+		type: 'boolean',
+	},
+	{
+		key: 'globalSoundEvents',
+		type: 'boolean',
+	},
+	{
+		key: 'keepInventory',
+		type: 'boolean',
+	},
+	{
+		key: 'lavaSourceConversion',
+		type: 'boolean',
+	},
+	{
+		key: 'locatorBar',
+		type: 'boolean',
+	},
+	{
+		key: 'logAdminCommands',
+		type: 'boolean',
+	},
+	{
+		key: 'maxCommandChainLength',
+		type: 'integer',
+	},
+	{
+		key: 'maxCommandForkCount',
+		type: 'integer',
+	},
+	{
+		key: 'maxEntityCramming',
+		type: 'integer',
+	},
+	{
+		key: 'mobExplosionDropDecay',
+		type: 'boolean',
+	},
+	{
+		key: 'mobGriefing',
+		type: 'boolean',
+	},
+	{
+		key: 'naturalRegeneration',
+		type: 'boolean',
+	},
+	{
+		key: 'playersNetherPortalCreativeDelay',
+		type: 'integer',
+	},
+	{
+		key: 'playersNetherPortalDefaultDelay',
+		type: 'integer',
+	},
+	{
+		key: 'playersSleepingPercentage',
+		type: 'integer',
+	},
+	{
+		key: 'projectilesCanBreakBlocks',
+		type: 'boolean',
+	},
+	{
+		key: 'pvp',
+		type: 'boolean',
+	},
+	{
+		key: 'randomTickSpeed',
+		type: 'integer',
+	},
+	{
+		key: 'reducedDebugInfo',
+		type: 'boolean',
+	},
+	{
+		key: 'sendCommandFeedback',
+		type: 'boolean',
+	},
+	{
+		key: 'showDeathMessages',
+		type: 'boolean',
+	},
+	{
+		key: 'snowAccumulationHeight',
+		type: 'integer',
+	},
+	{
+		key: 'spawnMonsters',
+		type: 'boolean',
+	},
+	{
+		key: 'spawnRadius',
+		type: 'integer',
+	},
+	{
+		key: 'spectatorsGenerateChunks',
+		type: 'boolean',
+	},
+	{
+		key: 'tntExplodes',
+		type: 'boolean',
+	},
+	{
+		key: 'tntExplosionDropDecay',
+		type: 'boolean',
+	},
+	{
+		key: 'universalAnger',
+		type: 'boolean',
+	},
+	{
+		key: 'waterSourceConversion',
+		type: 'boolean',
+	},
+] as const;
--- src/methods/GameRules.ts
+++ src/methods/GameRules.ts
@@ -1,5 +1,15 @@
+import { GameRules } from '../constants/GameRules.js';
 import { Client } from '../client/index.js';
-import { TypedGameRule, UntypedGameRule } from '../types/Minecraft.js';
+import { GameRuleType, TypedGameRule, UntypedGameRule } from '../types/Minecraft.js';
+
+type KnownGameRule = (typeof GameRules)[number];
+
+type RuleValue<T extends GameRuleType> = T extends 'boolean' ? boolean : T extends 'integer' ? number : string;
+
+type KnownUpdate<K extends KnownGameRule> = {
+	key: K['key'];
+	value: RuleValue<K['type']>;
+};
 
 export class GameRulesMethods {
 	private client: Client;
@@ -18,12 +28,21 @@ export class GameRulesMethods {
 	}
 
 	/**
-	 * Update a game rule value
+	 * Update a game rule value.
 	 *
-	 * @param {UntypedGameRule} gamerule - the game rule to update
-	 * @returns {TypedGameRule} the updated game rule with its typed value
+	 * @template K - A known game rule key
+	 * @param {KnownUpdate<K>} gamerule - A known game rule update (typed)
+	 * @param {UntypedGameRule} gamerule - An untyped game rule update
+	 * @returns {Promise<TypedGameRule>} The updated game rule with its typed value
 	 */
+	async update<K extends KnownGameRule>(gamerule: KnownUpdate<K>): Promise<TypedGameRule>;
+	async update(gamerule: UntypedGameRule): Promise<TypedGameRule>;
 	async update(gamerule: UntypedGameRule): Promise<TypedGameRule> {
-		return this.client.request<TypedGameRule>('minecraft:gamerules/update', [gamerule]);
+		const normalized: UntypedGameRule = {
+			...gamerule,
+			value: typeof gamerule.value === 'string' ? gamerule.value : String(gamerule.value),
+		};
+
+		return this.client.request<TypedGameRule>('minecraft:gamerules/update', [normalized]);
 	}
 }
